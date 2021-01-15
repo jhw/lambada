9577fe9524294b8cd40d27e9462d5eabcea0d59f
Justin <justin.worrall@gmail.com>
2021-01-15 05:54:43 +0000

    fixed input template and added max items to list events

--- scripts/list_events.sh
+++ scripts/list_events.sh
@@ -2,8 +2,14 @@
 
 if [ $# -eq 0 ]
 then
-    echo "Please enter app name"
+    echo "Please enter app name, max items"
     exit
 fi
 
-aws cloudformation describe-stack-events --stack-name $1 --query "StackEvents[].{\"1.Timestamp\":Timestamp,\"2.Id\":LogicalResourceId,\"3.Type\":ResourceType,\"4.Status\":ResourceStatus,\"5.Reason\":ResourceStatusReason}"
+if [ $# -eq 1 ]
+then
+    echo "Please enter app name, max items"
+    exit
+fi
+
+aws cloudformation describe-stack-events --stack-name $1 --query "StackEvents[].{\"1.Timestamp\":Timestamp,\"2.Id\":LogicalResourceId,\"3.Type\":ResourceType,\"4.Status\":ResourceStatus,\"5.Reason\":ResourceStatusReason}" --max-items $2
